32cc64bd30e8bf94d8a3118a3a0d65d7b5504af5
Mine Melodi Caliskan <minemelodicaliskan@gmail.com>
2019-07-08 14:43:28 +0300

    Update main.py

--- XGB/src/main.py
+++ XGB/src/main.py
@@ -3,10 +3,10 @@ import sys
 
 
 def main():
-    filename = sys.argv[1]
+    url = sys.argv[1]
     output_file = sys.argv[2]
     predicted_date = sys.argv[3]
-    model = ForecastRunner(filename, output_file, predicted_date)
+    model = ForecastRunner(url, output_file, predicted_date)
     df_test = model.fit()
     model.predict(df_test)
 
